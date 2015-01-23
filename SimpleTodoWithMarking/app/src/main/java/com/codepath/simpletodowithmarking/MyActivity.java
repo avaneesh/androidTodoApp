40a3d3138c62b9c2bd93a5cd8ef1584f3301bf4a
Avaneesh Kadam <avaneesh.kadam@gmail.com>
2015-01-22 23:30:29 -0800

    check box support

--- SimpleTodoWithMarking/app/src/main/java/com/codepath/simpletodowithmarking/MyActivity.java
+++ SimpleTodoWithMarking/app/src/main/java/com/codepath/simpletodowithmarking/MyActivity.java
@@ -1,14 +1,19 @@
 package com.codepath.simpletodowithmarking;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.apache.commons.io.FileUtils;
 
@@ -19,8 +24,8 @@ import java.util.ArrayList;
 
 public class MyActivity extends Activity {
 
-    ArrayList<String> items;
-    ArrayAdapter<String> itemsAdapter;
+    ArrayList<TodoItem> items;
+    MyCustomToDoAdapter itemsAdapter = null;
     ListView lvItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +33,9 @@ public class MyActivity extends Activity {
         setContentView(R.layout.activity_my);
 
         lvItems = (ListView) findViewById(R.id.lvItems);
-        //items = new ArrayList<String>();
         readItems();
-        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        itemsAdapter = new MyCustomToDoAdapter(this, R.layout.checked_list, items);
         lvItems.setAdapter(itemsAdapter);
-        //items.add("First item");
-        //items.add("Second item");
 
         setupListViewListener();
     }
@@ -55,7 +57,8 @@ public class MyActivity extends Activity {
     public void onAddItem(View v){
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
+        TodoItem todoItem = new TodoItem(itemText, false);
+        itemsAdapter.add(todoItem);
         etNewItem.setText("");
         writeItems();
     }
@@ -79,28 +82,150 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    /* Read item name and checked status from persistent storage */
     public void readItems(){
         File filesDir = getFilesDir();
         File todoFile = new File(filesDir, "todo.txt");
+        File todoCheckFile = new File(filesDir, "todo_check.txt");
+        ArrayList<String> itemsTextList;
+        ArrayList<String> itemsCheckList;
+        items = new ArrayList<TodoItem>();
 
         try{
-            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+            itemsTextList = new ArrayList<String>(FileUtils.readLines(todoFile));
+            itemsCheckList = new ArrayList<String>(FileUtils.readLines(todoCheckFile));
+
+            for (String s : itemsTextList) {
+                TodoItem todoItem = new TodoItem("new", false);
+                todoItem.setItemName(s.toString());
+                items.add(todoItem);
+            }
+            int idx=0;
+            for (String s : itemsCheckList) {
+                TodoItem todoItem = items.get(idx);
+                idx++;
+                if (s.toString().equals("true"))
+                    todoItem.setItemDone(true);
+            }
+
         }
         catch (IOException e){
-            items = new ArrayList<String>();
         }
+
     }
 
+
+    /* Write item name and checked status to persistent storage */
     public void writeItems(){
         File filesDir = getFilesDir();
         File todoFile = new File(filesDir, "todo.txt");
+        File todoCheckFile = new File(filesDir, "todo_check.txt");
+        ArrayList<String> itemsTextList = new ArrayList<String>();
+        ArrayList<String> itemsCheckList= new ArrayList<String>();
+
+        for (TodoItem i : items)
+            itemsTextList.add(new String(i.getItemName()));
+
+        for (TodoItem i : items)
+            if (i.isItemDone())
+                itemsCheckList.add("true");
+            else
+                itemsCheckList.add("false");
 
         try{
-            FileUtils.writeLines(todoFile, items);
+            FileUtils.writeLines(todoFile, itemsTextList);
+            FileUtils.writeLines(todoCheckFile, itemsCheckList);
         }
         catch (IOException e){
             e.printStackTrace();
         }
     }
 
+    private class MyCustomToDoAdapter extends ArrayAdapter<TodoItem> {
+
+        public MyCustomToDoAdapter (Context context, int textViewResourceId,
+                                    ArrayList<TodoItem> itemList) {
+            super(context, textViewResourceId, itemList);
+            items = new ArrayList<TodoItem>();
+            items.addAll(itemList);
+        }
+
+        @Override
+        public void add(TodoItem obj){
+            super.add(obj);
+            items.add(obj);
+        }
+
+        public class ViewHolder {
+            CheckBox doneCheck;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+
+            if (convertView == null){
+                LayoutInflater inflater =
+                        (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.checked_list, null);
+
+                holder = new ViewHolder();
+                holder.doneCheck = (CheckBox) convertView.findViewById(R.id.checkBox);
+                convertView.setTag(holder);
+
+                holder.doneCheck.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        CheckBox cb = (CheckBox) view;
+                        TodoItem todoItem = (TodoItem) cb.getTag();
+                        todoItem.setItemDone(cb.isChecked());
+                        writeItems();
+                    }
+                });
+            }
+            else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            if (items.size()>position) {
+                TodoItem todoItem = items.get(position);
+                holder.doneCheck.setVisibility(View.VISIBLE);
+                holder.doneCheck.setText(todoItem.getItemName());
+                holder.doneCheck.setChecked(todoItem.isItemDone());
+                holder.doneCheck.setTag(todoItem);
+            }
+            else{
+                holder.doneCheck.setVisibility(View.GONE);
+            }
+
+            return convertView;
+        }
+
+    }
+
+    public class TodoItem {
+        String itemName = null;
+        boolean itemDone = false;
+
+        public TodoItem (String name, boolean itemDone){
+            this.itemName = name;
+            this.itemDone = itemDone;
+        }
+
+        public boolean isItemDone(){
+            return itemDone;
+        }
+        public void setItemDone(boolean itemDone){
+            this.itemDone = itemDone;
+        }
+
+        public void setItemName(String itemName) {
+            this.itemName = itemName;
+        }
+
+        public String getItemName() {
+            return itemName;
+        }
+    }
 }
